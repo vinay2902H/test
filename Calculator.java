cd7d4c56b6ef11d2745bae13be85f921213a34f7
kothapallivinaykumar <165812777+vinay2902H@users.noreply.github.com>
2025-04-21 22:41:13 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Result: " + result);
+        System.out.println("Result: ");
     
     }
 }
